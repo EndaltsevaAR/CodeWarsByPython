42308a961781f2b37166dd76b3c65f3ce9ccd140
Aigul Endaltseva <lady.stranger2010@gmail.com>
2022-05-11 10:59:18 +0300

    6 kyu Moving Zeros To The End kata solution

--- _5kyu/moving_zeros_to_the_end.py
+++ _5kyu/moving_zeros_to_the_end.py
@@ -7,10 +7,11 @@ move_zeros([1, 0, 1, 2, 0, 1, 3]) # returns [1, 1, 2, 1, 3, 0, 0]
 
 def move_zeros(array):
     zeros = [0] * array.count(0)
+    not_nulls = list()
     for numb in array:
-        if numb == 0:
-            array.remove(numb)
-    return array + zeros
+        if numb != 0:
+            not_nulls.append(numb)
+    return not_nulls + zeros
 
 
-print(move_zeros([1, 2]))
+print(move_zeros([9, 0, 0, 9, 1, 2, 0, 1, 0, 1, 0, 3, 0, 1, 9, 0, 0, 0, 0, 9]))
