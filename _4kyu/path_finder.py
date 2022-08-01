3ea8aaf611d1dc3c4bac2835662c2a124484e25b
Aigul Endaltseva <lady.stranger2010@gmail.com>
2022-08-01 23:16:41 +0300

    4 kyu Path Finder #1: can you reach the exit? kata solving before debagging

---
+++ _4kyu/path_finder.py
@@ -0,0 +1,31 @@
+
+def path_finder(maze):
+    lines = maze.split('\n')
+    graph = [[False] * len(lines) for i in range(len(lines))]
+    for y in range(len(lines)):
+        for x in range(len(lines)):
+            if list(lines[y])[x] == 'W':
+                graph[y][x] = True
+            else:
+                graph[y][x] = False
+    go_to(graph, 0, 0)
+    return graph[len(graph) - 1][len(graph) - 1]
+
+
+def go_to(graph, y, x):
+    if y == -1 or y == len(graph) or x == -1 or x == len(graph) or graph[y][x]:
+        return
+    graph[y][x] = True
+    go_to(graph, y, x - 1)
+    go_to(graph, y, x + 1)
+    go_to(graph, y - 1, x)
+    go_to(graph, y + 1, x)
+
+
+a = "\n".join([
+    ".W...",
+    ".W...",
+    ".W.W.",
+    "...WW",
+    "...W."])
+print(path_finder(a))
