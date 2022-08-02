d039125c079ac7dacedb1b24f4edb4c873108802
Aigul Endaltseva <lady.stranger2010@gmail.com>
2022-08-02 22:08:06 +0300

    4 kyu Path Finder #1: change graph to global variable

--- _4kyu/path_finder.py
+++ _4kyu/path_finder.py
@@ -1,6 +1,9 @@
+graph = [[]]
+
 
 def path_finder(maze):
     lines = maze.split('\n')
+    global graph
     graph = [[False] * len(lines) for i in range(len(lines))]
     for y in range(len(lines)):
         for x in range(len(lines)):
@@ -8,18 +11,18 @@ def path_finder(maze):
                 graph[y][x] = True
             else:
                 graph[y][x] = False
-    go_to(graph, 0, 0)
+    go_to(0, 0)
     return graph[len(graph) - 1][len(graph) - 1]
 
 
-def go_to(graph, y, x):
+def go_to(y, x):
     if y == -1 or y == len(graph) or x == -1 or x == len(graph) or graph[y][x]:
         return
     graph[y][x] = True
-    go_to(graph, y, x - 1)
-    go_to(graph, y, x + 1)
-    go_to(graph, y - 1, x)
-    go_to(graph, y + 1, x)
+    go_to(y, x - 1)
+    go_to(y, x + 1)
+    go_to(y - 1, x)
+    go_to(y + 1, x)
 
 
 a = "\n".join([
